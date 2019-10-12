fff3d2093fc1a3319f15ddaba2113d961b143486
mace <mace_blue@hotmail.com>
2019-10-12 19:20:43 +0200

    remove debug

--- admin/save.php
+++ admin/save.php
@@ -30,7 +30,7 @@ $statement		= $_REQUEST['statement'];
 $sale			= $_REQUEST['sale'];
 
 $values = [$categorie,$name,$price,$length,$width,$depth,$year,$material,$hoursused,$cabines,$motorcompany,$motorcount,$power,$oiltank,$watertank,$status,$location,$equipment,$sail,$arrangement,$pantry,$instruments,$roof,$etc,$statement,$sale];
-print_r($values); exit;
+
 $pdo = new PDO('mysql:host='.$host.'; dbname='.$database, $user, $pass);
 
 if($id==""){ #neues Boot anlegen
